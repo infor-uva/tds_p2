c3ba41d5acebb0a9cf73189b9dff99b942779f03
migudel <miguel.moras@estudiantes.uva.es>
2023-11-25 11:39:55 +0100

    Eliminada importación innecesaria

--- src/main/java/uva/tds/practica2_grupo6/SistemaPersistencia.java
+++ src/main/java/uva/tds/practica2_grupo6/SistemaPersistencia.java
@@ -3,7 +3,6 @@ package uva.tds.practica2_grupo6;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
