6a4d724dcf1549f0049453f8d0c09cf2e8724cdc
migudel <miguel.moras@estudiantes.uva.es>
2023-11-28 12:56:22 +0100

    Eliminación importe innecesario

--- src/test/java/uva/tds/practica2_grupo6/SistemaPersistenciaTest.java
+++ src/test/java/uva/tds/practica2_grupo6/SistemaPersistenciaTest.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.easymock.EasyMock;
